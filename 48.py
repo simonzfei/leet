ac4433d891ecb96ca64b106dfb4551af84573746
zfei <simoncsf@outlook.com>
2024-08-05 16:37:51 +0800

    "08"

--- 48.py
+++ 48.py
@@ -45,13 +45,13 @@
 """
 按层次进行旋转，每次处理矩阵的外层。
 对每一层进行4个角的交换。
-"""
+
 class Solution(object):
     def rotate(self, matrix):
-        """
+        
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
-        """
+        
         n = len(matrix)
         self.rotate_layer(matrix, 0, n)
 
@@ -85,4 +85,40 @@ matrix2 = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
 print("Before rotation:", matrix2)
 Solution().rotate(matrix2)
 print("After rotation:", matrix2)  # 输出: [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+"""
+
+"""
+先进行转置操作。
+再进行每一行的反转操作。
+
+"""
+
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+
+        # Step 1: Transpose the matrix
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Step 2: Reverse each row
+        for i in range(n):
+            matrix[i].reverse()
+
+# 示例 1
+matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+Solution().rotate(matrix1)
+print(matrix1)  # 输出: [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+# 示例 2
+matrix2 = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+Solution().rotate(matrix2)
+print(matrix2)  # 输出: [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
+
+
 
